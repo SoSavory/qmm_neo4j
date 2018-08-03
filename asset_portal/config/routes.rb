e32bfe8f4ed2dd52a1c60d2d8e27f5199b5e8e3e
Avery <11986400+SoSavory@users.noreply.github.com>
2018-08-03 10:26:45 -0400

    fix for importing specific

--- asset_portal/config/routes.rb
+++ asset_portal/config/routes.rb
@@ -11,6 +11,11 @@ Rails.application.routes.draw do
   resources :users
     get 'user/get_curated_articles_pagination_splines', to: 'users#get_user_curated_articles_pagination_splines', as: :get_user_curated_articles_pagination_splines
     get 'user/get_curated_articles', to: 'users#get_user_curated_articles', as: :get_user_curated_articles
+    get '/import', to: 'users#import', as: :import
+    post '/parse_import', to: 'users#parse_import', as: :parse_import
+
+    get '/import_specific', to: 'users#import_specific', as: :import_specific
+    post '/parse_import_specific', to: 'users#parse_import_specific', as: :parse_import_specific
   resources :raw_articles
     get 'raw_article/get_pagination_splines', to: 'raw_articles#get_pagination_splines', as: :get_raw_article_pagination_splines
     get 'raw_article/curate/:id', to: 'raw_articles#curate_raw_article', as: :curate_raw_article
@@ -18,11 +23,7 @@ Rails.application.routes.draw do
 
 
 
-  get '/import', to: 'users#import', as: :import
-  post '/parse_import', to: 'users#parse_import', as: :parse_import
 
-  get '/import_specific', to: 'users#import_specific', as: :import_specific
-  post '/parse_import_specific', to: 'users#parse_import_specific', as: :parse_import_specific
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
